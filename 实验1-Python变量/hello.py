fae0eaf2caf5bb1346d71987dfa40922dfd66fc7
jason <zhoujing204@outlook.com>
2025-02-21 13:32:53 +0800

    minor changes

--- 实验1-Python变量/hello.py
+++ 实验1-Python变量/hello.py
@@ -1,9 +1,1 @@
 print('Hello World')
-
-# from ..util import notebook2pdf
-# notebook_file = "实验一-Python变量.ipynb"
-# html_file = "notebook.html"
-# pdf_file = "output.pdf"
-
-# notebook2pdf.notebook_to_html(notebook_file, html_file)
-# notebook2pdf.html_to_pdf(html_file, pdf_file)
